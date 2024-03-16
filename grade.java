b5e68b1fe2d83bd4e02045dc5df474d634207f9a
anonymous0124 <itachiuchiha200419@gmail.com>
2024-03-16 11:54:28 +0545

    changes in grade

--- grade.java
+++ grade.java
@@ -1,29 +1,24 @@
 import java.util.Scanner;
 public class grade {
-public static void main(String[]argv)
-{
-    Scanner ob= new Scanner(System.in);
-    int grade;
-    System.out.println("enter you grade:");
-    grade=ob.nextInt();
-    if(grade<25){
-        System.out.println("you are fail");
-    }
-        if(grade<45){
-            System.out.println("you have obtained E grade");
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int grade;
+        System.out.println("Enter your grade:");
+        grade = ob.nextInt();
+
+        if (grade < 25) {
+            System.out.println("You have failed.");
+        } else if (grade >= 25 && grade < 45) {
+            System.out.println("You have obtained E grade.");
+        } else if (grade >= 45 && grade < 50) {
+            System.out.println("You have obtained D grade.");
+        } else if (grade >= 50 && grade < 60) {
+            System.out.println("You have obtained C grade.");
+        } else if (grade >= 60 && grade < 80) {
+            System.out.println("You have obtained B grade.");
+        } else {
+            System.out.println("You have obtained A grade.");
         }
-            if(grade<50){
-                System.out.println("you have obtained D grade");
-            }
-                if(grade<60){
-                    System.out.println("you have obtained C grade");
-                }
-                    if(grade<80){
-                        System.out.println("you have obtaibed B grade");
-                    }
-                        if (grade>80) {
-                            System.out.println("you have obtained A grade");
-                        }
-    ob.close();
-}
+        ob.close();
+    }
 }
